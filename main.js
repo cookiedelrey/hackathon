8c3a35a3f8737a2ab0852b3b36a438a4b176a68d
cookiedelrey <aikanysh.akynbekova.t@gmail.com~>
2022-11-07 15:58:00 +0600

    commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const API = "http://localhost:8000/elements";
 
 let name = document.querySelector('#name');
 let type = document.querySelector('#type');
+let status = document.querySelector('#status');
 let link = document.querySelector('#doclink');
 let btnAdd = document.querySelector('#btn-add');
 
@@ -18,6 +19,7 @@ let next = document.querySelector(".next");
 
 let editName = document.querySelector("#edit-name");
 let editType = document.querySelector("#edit-type");
+let editStatus = document.querySelector('#edit-status')
 let editLink = document.querySelector("#edit-link");
 let editSaveBtn = document.querySelector("#btn-save-edit");
 let modal = document.querySelector("#modal");
@@ -26,11 +28,13 @@ btnAdd.addEventListener("click", async function () {
     let obj = {
       name: name.value,
       type: type.value,
+      status: status.value,
       link: link.value,
     };
     if (
       !obj.name.trim() ||
       !obj.type.trim() ||
+      !obj.status.trim() ||
       !obj.link.trim()
     ) {
       alert("Fill the area!");
@@ -46,6 +50,7 @@ btnAdd.addEventListener("click", async function () {
     
       name.value = "";
       type.value = "";
+      status.value = "";
       link.value = "";
       render();
 });
@@ -61,13 +66,14 @@ async function render() {
     documents.forEach((element) => {
       let newElem = document.createElement("div");
       newElem.id = element.id;
-      newElem.innerHTML = `<div class="card m-3" style="width: 18rem;">
-          <img src=${element.link} class="card-img-top" alt="img">
+      newElem.innerHTML = `<div class="card m-3" style="width: 15rem; background-color: rgb(155, 197, 155)">
+      <img src=${element.link} class="card-img-top" alt="img">
               <div class="card-body">
                   <h5 class="card-title">${element.name}</h5>
                   <p class="card-text">${element.type}</p>
+                  <p class="card-text">${element.status}</p>
                   <a href="#" id=${element.id} onclick='deleteProduct(${element.id})' class="btn btn-outline-success btn-delete" >Delete</a>
-                  <a href="#" id=${element.id} class="btn btn-outline-success btn-edit" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</a>
+                  <a href="#" id=${element.id} class="btn btn-outline-success btn-edit" data-bs-toggle="modal" data-bs-target="#modal">Edit</a>
               </div>
           </div>`;
       list.append(newElem);
@@ -85,13 +91,13 @@ async function render() {
         for (let i = 1; i <= pageTotal; i++) {
           if (currentPage == i) {
             let page1 = document.createElement("li");
-            page1.innerHTML = ` <li class="page-item"><a class="page-link page-number text-sucsess" href="#">${i}</a></li>`;
+            page1.innerHTML = ` <li class="page-item"><a class="page-link page-number text-success" href="#">${i}</a></li>`;
             paginationList.append(page1);
           } else {
             let page1 = document.createElement("li");
-            page1.innerHTML = ` <li class="page-item"><a class="page-link page-number" href="#">${i}</a></li>`;
+            page1.innerHTML = ` <li class="page-item"><a class="page-link page-number text-success" href="#">${i}</a></li>`;
             paginationList.append(page1);
-          }
+          } 
         }
   
         if (currentPage == 1) {
@@ -131,41 +137,36 @@ async function render() {
     }
   });
   
-  document.addEventListener("click", (e) => {
-      if (e.target.classList.contains("btn-delete")) {
-          let id = e.target.id;
-          fetch(`${API}/${id}`, { method: "DELETE" }).then(() => render());
-      }
-  });
-  
   function deleteProduct(id) {
     fetch(`${API}/${id}`, { method: "DELETE" }).then(() => render());
   }
 
-  document.addEventListener("click", function (e) {
+  document.addEventListener("click", function(e) {
     if (e.target.classList.contains("btn-edit")) {
       let id = e.target.id;
+      console.log(id);
       fetch(`${API}/${id}`).then((res) =>
         res.json().then((data) => {
           editName.value = data.name;
           editType.value = data.type;
+          editStatus.value = data.status;
           editLink.value = data.link;
-  
+
           editSaveBtn.setAttribute("id", data.id);
         })
       );
     }
   });
   
-  //! сохранение изменений товара
   editSaveBtn.addEventListener("click", function () {
     let id = this.id;
     let name = editName.value;
     let type = editType.value;
+    let status = editStatus.value;
     let link = editLink.value;
 
   
-    if (!name.trim() || !type.trim() || !link.trim()) {
+    if (!name.trim() || !type.trim() || !status.trim() || !link.trim()) {
       alert("Fill the area!");
       return;
     }
@@ -173,6 +174,7 @@ async function render() {
     let editedProduct = {
         name: name,
         type: type,
+        status: status,
         link: link
     };
     console.log(editedProduct);
@@ -186,8 +188,9 @@ async function render() {
       body: JSON.stringify(editedProduct),
     }).then(() => render());
   
-    let modal = bootstrap.Modal.getInstance(exampleModal);
-    modal.hide();
+    let exampleModal = bootstrap.Modal.getInstance(modal);
+    console.log(exampleModal);
+    exampleModal.hide();
   }
 
   searchInp.addEventListener("input", () => {
